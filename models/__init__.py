c8b2248cb0841f1e1c0ff89f061aa6b99f06c92a
Sybren <sybren@nerbys.nl>
2025-12-16 15:31:01 +0000

    Feature: Direct margin config on categories + dashboard tiles
    - Added margin_percentage field directly on product.public.category
    - Automatic sync with product.margin.config
    - Easy margin setup in webshop categories view
    - Added 2 dashboard tiles to webshop catalog dashboard
    - Tile 1: Products without margin config (warning)
    - Tile 2: Products with margin deviation (info)
    - Both tiles have 'Bekijk' buttons to view filtered products

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,5 @@
 
 from . import product_margin_config
 from . import product_template
+from . import product_public_category
+from . import webshop_catalog_dashboard
---
+++ models/product_public_category.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ProductPublicCategory(models.Model):
+    _inherit = 'product.public.category'
+
+    margin_percentage = fields.Float(
+        string='Standaard Marge %',
+        help='Standaard marge percentage voor producten in deze categorie'
+    )
+    margin_config_id = fields.Many2one(
+        'product.margin.config',
+        string='Marge Configuratie',
+        compute='_compute_margin_config',
+        store=True
+    )
+    has_margin_config = fields.Boolean(
+        string='Heeft Marge Config',
+        compute='_compute_margin_config',
+        store=True
+    )
+
+    @api.depends('margin_percentage')
+    def _compute_margin_config(self):
+        """Zoek of maak marge configuratie voor deze categorie"""
+        MarginConfig = self.env['product.margin.config']
+        
+        for category in self:
+            # Zoek bestaande configuratie
+            config = MarginConfig.search([
+                ('config_type', '=', 'category'),
+                ('public_categ_id', '=', category.id)
+            ], limit=1)
+            
+            category.margin_config_id = config
+            category.has_margin_config = bool(config)
+
+    def write(self, vals):
+        """Update of maak marge configuratie bij wijziging percentage"""
+        result = super().write(vals)
+        
+        if 'margin_percentage' in vals:
+            MarginConfig = self.env['product.margin.config']
+            
+            for category in self:
+                if category.margin_percentage > 0:
+                    # Zoek bestaande config
+                    config = MarginConfig.search([
+                        ('config_type', '=', 'category'),
+                        ('public_categ_id', '=', category.id)
+                    ], limit=1)
+                    
+                    if config:
+                        # Update bestaande
+                        config.margin_percentage = category.margin_percentage
+                    else:
+                        # Maak nieuwe
+                        MarginConfig.create({
+                            'name': f'Marge {category.name}',
+                            'config_type': 'category',
+                            'public_categ_id': category.id,
+                            'margin_percentage': category.margin_percentage,
+                        })
+                elif category.margin_config_id:
+                    # Verwijder config als marge op 0 gezet
+                    category.margin_config_id.unlink()
+        
+        return result
