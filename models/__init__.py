0d8927d5e476efb978986dbdd318e0e3f4c218f6
Sybren <sybren@nerbys.nl>
2025-12-17 20:41:08 +0100

    Clean up __init__.py - remove dashboard import

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,3 @@
 from . import product_margin_config
 from . import product_template
 from . import product_public_category
-
-try:
-    from . import webshop_catalog_dashboard
-except ImportError:
-    pass
